d096a2db49d0ea12d06e768fc065c60c0124d4d9
a1518915457 <1518915457@qq.com>
2018-08-28 15:41:28 +0800

    Add MD5 hash module.

---
+++ ArkSdk/Util/Hash.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkSdk.Util
+{
+    class Hash
+    {
+        public static string Md5(string s)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(s));
+
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }
+        }
+    }
+}
