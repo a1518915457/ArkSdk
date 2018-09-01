b0ca0489061ad1f86f49a9668663462965f32c52
a1518915457 <1518915457@qq.com>
2018-09-01 16:29:16 +0800

    添加订单详情请求模板

---
+++ ArkSdk/Request/PackageGetRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkSdk.Request
+{
+    public class PackageGetRequest : BaseArkRequest<Response.PackageGetResponse>
+    {
+        public string PackageId { get; set; }
+
+        public override string GetMethod()
+        {
+            return "GET";
+        }
+
+        public override IDictionary<string, string> GetParameters()
+        {
+            ArkDictionary parameters = new ArkDictionary();
+            return parameters;
+        }
+
+        public override string GetUrl()
+        {
+            return "/ark/open_api/v0/packages/" + PackageId;
+        }
+    }
+}
